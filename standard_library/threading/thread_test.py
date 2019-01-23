02880a4714f8fb2dc6e7cabd57679b02a69af413
Jared Nishikawa <jnishikawa@carbonblack.com>
2019-01-23 15:11:32 -0700

    Adjusted threading example

--- standard_library/threading/thread_test.py
+++ standard_library/threading/thread_test.py
@@ -3,12 +3,12 @@ import time
 
 def powers(p):
     for x in range(10):
-        print(p**x)
-        print(threading.current_thread().name)
+        print(threading.current_thread().name, ": ", p**x, sep='')
         time.sleep(1)
 
 if __name__ == '__main__':
     threads = []
     for p in [2, 3, 5, 7, 11]:
         t = threading.Thread(target=powers, args=(p,))
+        t.setName("Thread(p={})".format(p))
         t.start()
